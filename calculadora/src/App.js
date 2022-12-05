e3f75eb44a69a0baf85b415a4718b9b62b6b6f05
AmandaSugimoto <99562749+AmandaSugimoto@users.noreply.github.com>
2022-12-05 20:45:45 -0300

    up

---
+++ calculadora/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+import Calculadora from './calculadora';
+
+function App() {
+  return (
+    <div>
+      <Calculadora/>
+    </div>
+  );
+}
+
+export default App;
